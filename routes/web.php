3a25c1422ce79aaf635b7776a6835d6225a12757
Adam K <akalata@sympatico.ca>
2017-05-15 21:05:46 +0000

    learned controllers

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/tasks', function () {
+Route::get('/tasks', 'TasksController@index');
 
-	$tasks = App\Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-
-	$task = App\Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
-
-
-
-// Route::get('/', function (){
-
-//     return view('welcome');
-// });
-
+Route::get('/tasks/{task}', 'TasksController@show');
